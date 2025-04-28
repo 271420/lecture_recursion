c0bc9a7f122845a42cdacc27a17566845b6a3646
Eliska Uncovska <271420@vutbr.cz>
2025-04-28 14:25:30 +0200

    m

--- fibo.py
+++ fibo.py
@@ -1,5 +1,16 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
 
 def main():
+    number = int(input("pocet clenu fibonaciho posloupnosti:  "))
+    print(recursive_nth_fibo(number))
+    seq = [recursive_nth_fibo(num) for num in range(number + 1)]
+    print(seq)
     pass
 
 
